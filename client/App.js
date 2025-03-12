1031f98b301ff7562b3fc13ef9db5d95ed1da71f
ping_z <ping90750@gmail.com>
2025-03-13 01:45:27 +0700

    subscribeToTopic notification

--- client/App.js
+++ client/App.js
@@ -3,8 +3,7 @@ import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigation from "./navigation";
 import { useFonts } from "expo-font";
-// import * as Notifications from "expo-notifications";
-// import messaging from "@react-native-firebase/messaging";
+import messaging from "@react-native-firebase/messaging";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -13,32 +12,9 @@ export default function App() {
     // สามารถเพิ่มฟอนต์อื่น ๆ ตามต้องการได้ที่นี่
   });
 
-  // useEffect(() => {
-  //   registerForPushNotifications();
-
-  //   // Handle foreground notifications
-  //   const unsubscribe = messaging().onMessage(messaging, (payload) => {
-  //     console.log("Message received: ", payload);
-  //     Alert.alert("New Notification", payload.notification.body);
-  //   });
-
-  //   return unsubscribe;
-  // }, []);
-
-  // const registerForPushNotifications = async () => {
-  //   const { status } = await Notifications.requestPermissionsAsync();
-  //   if (status !== "granted") {
-  //     Alert.alert("Permission Denied", "You need to enable notifications.");
-  //     return;
-  //   }
-
-  //   // รับ Expo Push Token
-  //   const token = await messaging().getToken();
-
-  //   // const token = (await Notifications.getExpoPushTokenAsync()).data;
-  //   console.log("Push Token:", token);
-  //   // setExpoPushToken(token);
-  // };
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log("Message handled in the background!", remoteMessage);
+  });
 
   return (
     <NavigationContainer>
